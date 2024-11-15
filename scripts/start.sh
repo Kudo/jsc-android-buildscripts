8e6f42f2010efe7b10170929e80ccaadbd051ca2
Kudo Chien <ckchien@gmail.com>
2024-11-15 18:42:54 +0800

    test

--- scripts/start.sh
+++ scripts/start.sh
@@ -108,20 +108,20 @@ export DISTDIR=${ROOTDIR}/dist
 printf "\n\n\t\t===================== create stripped distributions =====================\n\n"
 createAAR "android-jsc" ${DISTDIR} ${INSTALL_DIR_I18N_false} "false"
 createAAR "android-jsc" ${DISTDIR} ${INSTALL_DIR_I18N_true} "true"
-# createAAR "cppruntime" ${DISTDIR} ${INSTALL_CPPRUNTIME_DIR} "false"
 printf "\n\n\t\t===================== ooxx =====================\n\n"
 find ${INSTALL_CPPRUNTIME_DIR}
 printf "\n\n\t\t===================== ooxx =====================\n\n"
+createAAR "cppruntime" ${DISTDIR} ${INSTALL_CPPRUNTIME_DIR} "false"
 copyHeaders ${DISTDIR}
 
 printf "\n\n\t\t===================== create unstripped distributions =====================\n\n"
 export DISTDIR=${ROOTDIR}/dist.unstripped
 createAAR "android-jsc" ${DISTDIR} ${INSTALL_UNSTRIPPED_DIR_I18N_false} "false"
 createAAR "android-jsc" ${DISTDIR} ${INSTALL_UNSTRIPPED_DIR_I18N_true} "true"
-# createAAR "cppruntime" ${DISTDIR} ${INSTALL_CPPRUNTIME_DIR} "false"
 printf "\n\n\t\t===================== ooxx =====================\n\n"
 find ${INSTALL_CPPRUNTIME_DIR}
 printf "\n\n\t\t===================== ooxx =====================\n\n"
+createAAR "cppruntime" ${DISTDIR} ${INSTALL_CPPRUNTIME_DIR} "false"
 copyHeaders ${DISTDIR}
 
 npm run info
